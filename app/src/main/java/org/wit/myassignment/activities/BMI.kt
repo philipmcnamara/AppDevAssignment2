070e583f10c368e021a2fe80815ad7ad55bf1d29
Philip McNamara <philipmcnamara1@gmail.com>
2021-11-24 18:16:53 +0000

    xml built, bugs everywhere

---
+++ app/src/main/java/org/wit/myassignment/activities/BMI.kt
@@ -0,0 +1,12 @@
+package org.wit.myassignment.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.myassignment.R
+
+class BMI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi)
+    }
+}
