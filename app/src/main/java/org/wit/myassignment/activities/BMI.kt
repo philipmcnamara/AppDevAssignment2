41bfaa21799b4075f7ce5f5ffa99e7aecbad8608
Philip McNamara <philipmcnamara1@gmail.com>
2021-11-27 12:57:44 +0000

    seekbar method working...ish

--- app/src/main/java/org/wit/myassignment/activities/BMI.kt
+++ app/src/main/java/org/wit/myassignment/activities/BMI.kt
@@ -2,6 +2,7 @@ package org.wit.myassignment.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_bmi.*
 import org.wit.myassignment.R
@@ -13,19 +14,37 @@ class BMI : AppCompatActivity() {
 
     lateinit var app: MainApp
     private lateinit var binding: ActivityBmiBinding
+    private var seekBar: SeekBar? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityBmiBinding.inflate(layoutInflater)
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi)
+
+        height_value
+
+        seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                height_value.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
     }
 
+
+
     fun increaseAge(view: android.view.View) {
-        Timber.i("Increase Button Pressed:")
+
         if (Integer.parseInt(age.text.toString()) > 0){
             age.text = (Integer.parseInt(age.text as String)).plus(1).toString()
-            Timber.i("Age : ${age.text}")
         }
     }
 
