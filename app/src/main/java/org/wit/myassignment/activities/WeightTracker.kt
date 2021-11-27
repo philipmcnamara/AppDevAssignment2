77b46c1108bf4bc75f4730906c24fd5a20c31253
Philip McNamara <philipmcnamara1@gmail.com>
2021-11-27 17:13:29 +0000

    basic graph with hardcoded values in

---
+++ app/src/main/java/org/wit/myassignment/activities/WeightTracker.kt
@@ -0,0 +1,47 @@
+package org.wit.myassignment.activities
+
+import android.graphics.Color
+import android.graphics.Color.BLUE
+import android.hardware.camera2.params.RggbChannelVector.BLUE
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidplot.xy.*
+import kotlinx.android.synthetic.main.activity_weightgraph.*
+import org.wit.myassignment.R
+import java.text.FieldPosition
+import java.text.Format
+import java.text.ParsePosition
+import java.util.*
+
+class WeightTracker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weight_tracker)
+
+        val domainLabels = arrayOf<Number>(1,2,3,4,5,6,7,8,9,10)
+        val series1Number = arrayOf<Number>(1,4,8,16,32,26,29,10,13)
+
+        val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* series1Number), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Serires 1")
+
+        val series1Format = LineAndPointFormatter(Color.BLUE, Color.BLACK, null, null)
+
+        plot.addSeries(series1,series1Format)
+
+        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
+            override fun format(
+                obj: Any?,
+                toAppendTo: StringBuffer?,
+                pos: FieldPosition?
+            ): StringBuffer? {
+                val i = Math.round((obj as Number).toFloat())
+                return toAppendTo?.append(domainLabels[i])
+            }
+
+            override fun parseObject(source: String?, pos: ParsePosition?): Any? {
+                return null
+            }
+
+        }
+        PanZoom.attach(plot)
+    }
+}
