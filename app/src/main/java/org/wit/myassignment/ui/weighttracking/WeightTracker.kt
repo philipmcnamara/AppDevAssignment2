4b257735d6112f3055ab82140e5493df40d414ed
Philip McNamara <philipmcnamara1@gmail.com>
2021-12-16 16:34:33 +0000

    fireData stuck in ValueEventListener - update

--- app/src/main/java/org/wit/myassignment/ui/weighttracking/WeightTracker.kt
+++ app/src/main/java/org/wit/myassignment/ui/weighttracking/WeightTracker.kt
@@ -24,13 +24,17 @@ class WeightTracker : AppCompatActivity() {
 
     private lateinit var database : DatabaseReference
 
+    var fireData = ArrayList<WeightData>()
+    var daysList = arrayListOf<Int>()
+    var weightList = arrayListOf<Int>()
+    var increment = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.weight_tracker)
 
         database = FirebaseDatabase.getInstance().getReference("weightData").child("weights")
-        var fireData = arrayListOf<WeightData>()
+        //var fireData = arrayListOf<WeightData>()
 
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -39,57 +43,61 @@ class WeightTracker : AppCompatActivity() {
                     fireData.add(data!!)
                 }
                 //this fireData contains a full arrayList
-                System.out.println(fireData)
+                //System.out.println(fireData)
+                // Graph will not work in here!!
+                populateList(fireData)
+
+
             }
             override fun onCancelled(databaseError: DatabaseError) {
                 throw databaseError.toException()
             }
         })
+            //Graph will work here but the Data will not get sent into it!
+            //System.out.println(fireData)
 
-        var daysList = arrayListOf<Int>()
-        val weightList = arrayListOf<Int>()
-        var increment = 0
-
-        //firebase data here is and empty array, Need to call the data from line 39
-        for (it in fireData){
-            daysList.add(it.dayOfMeasurement!!.toInt())
-            weightList.add(it.currentWeight!!.toInt())
-            increment = ++increment
-            System.out.println(fireData)
-        }
-        //System.out.println(fireData)
-
-        //val daysArray: Array<Int> = daysList.toTypedArray()
-        //val weightArray: Array<Int> = weightList.toTypedArray()
-
-       val daysArray = arrayListOf(19,2,3,4,5,6,7,8,9,10)
-        val weightArray = arrayOf(84,82,81,83,86,85,81,79,75)
-        //System.out.println(daysArray[0])
-       // System.out.println(weightArray[1])
+           // val daysArray: Array<Int> = daysList.toTypedArray()
+            //val weightArray: Array<Int> = weightList.toTypedArray()
+            val daysArray = arrayListOf(1,2,3,4,5,6,7,8,9,10)
+            val weightArray = arrayOf(84,82,81,83,86,85,81,79,75)
+            //System.out.println(daysArray[0])
+            // System.out.println(weightArray[1])
 
 
-        val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* weightArray), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Days Tracked")
+            val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* weightArray), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Days Tracked")
 
-        val series1Format = LineAndPointFormatter(Color.BLUE, Color.BLACK, null, null)
+            val series1Format = LineAndPointFormatter(Color.BLUE, Color.BLACK, null, null)
 
-        plot.addSeries(series1,series1Format)
+            plot.addSeries(series1,series1Format)
 
-        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
-            override fun format(
-                obj: Any?,
-                toAppendTo: StringBuffer?,
-                pos: FieldPosition?
-            ): StringBuffer? {
-                val i = Math.round((obj as Number).toFloat())
-                return toAppendTo?.append(daysArray[i])
+            plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
+                override fun format(
+                    obj: Any?,
+                    toAppendTo: StringBuffer?,
+                    pos: FieldPosition?
+                ): StringBuffer? {
+                    val i = Math.round((obj as Number).toFloat())
+                    return toAppendTo?.append(daysArray[i])
+                }
+
+                override fun parseObject(source: String?, pos: ParsePosition?): Any? {
+                    return null
+                }
             }
-
-            override fun parseObject(source: String?, pos: ParsePosition?): Any? {
-                return null
-            }
-        }
-        PanZoom.attach(plot)
+            PanZoom.attach(plot)
     }
+
+
+    fun populateList(fireData : ArrayList<WeightData>){
+        //firebase data here is and empty array, Need to call the data from line 39
+        for (it in fireData) {
+            daysList.add(it.dayOfMeasurement!!.toInt())
+           // weightList.add(it.currentWeight!!.toInt())
+            //System.out.println(fireData)
+        }
+    }
+
+
 
     fun Home(view: android.view.View) {
         val intent = Intent(this, WeightList::class.java)
