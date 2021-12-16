2234cfb2731161a271c95c66a7fd5337d3611a33
Philip McNamara <philipmcnamara1@gmail.com>
2021-12-16 14:45:21 +0000

    fireData stuck in ValueEventListener

--- app/src/main/java/org/wit/myassignment/ui/weighttracking/WeightTracker.kt
+++ app/src/main/java/org/wit/myassignment/ui/weighttracking/WeightTracker.kt
@@ -17,58 +17,58 @@ import java.text.FieldPosition
 import java.text.Format
 import java.text.ParsePosition
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class WeightTracker : AppCompatActivity() {
 
     private lateinit var database : DatabaseReference
-    var weights = ArrayList<WeightData>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.weight_tracker)
 
         database = FirebaseDatabase.getInstance().getReference("weightData").child("weights")
+        var fireData = arrayListOf<WeightData>()
 
-        val fireData = arrayListOf<WeightData>()
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (productSnapshot in dataSnapshot.children) {
-                    val product = productSnapshot.getValue(WeightData::class.java)
-                    fireData.add(product!!)
+                    var data = productSnapshot.getValue(WeightData::class.java)
+                    fireData.add(data!!)
                 }
-
-                var daysTracked = arrayListOf<Int>()
-                val weightList = arrayListOf<Int>()
-                var increment = 0
-
-                for (it in fireData){
-                    daysTracked.add(it.dayOfMeasurement!!.toInt())
-                    weightList.add(it.currentWeight!!.toInt())
-
-                    increment = ++increment
-                   // System.out.println(aysTracked)
-
-                }
-
-                System.out.println(daysTracked)
-                System.out.println(weightList)
-
+                //this fireData contains a full arrayList
+                System.out.println(fireData)
             }
-
-
             override fun onCancelled(databaseError: DatabaseError) {
                 throw databaseError.toException()
             }
         })
 
+        var daysList = arrayListOf<Int>()
+        val weightList = arrayListOf<Int>()
+        var increment = 0
+
+        //firebase data here is and empty array, Need to call the data from line 39
+        for (it in fireData){
+            daysList.add(it.dayOfMeasurement!!.toInt())
+            weightList.add(it.currentWeight!!.toInt())
+            increment = ++increment
+            System.out.println(fireData)
+        }
+        //System.out.println(fireData)
+
+        //val daysArray: Array<Int> = daysList.toTypedArray()
+        //val weightArray: Array<Int> = weightList.toTypedArray()
+
+       val daysArray = arrayListOf(19,2,3,4,5,6,7,8,9,10)
+        val weightArray = arrayOf(84,82,81,83,86,85,81,79,75)
+        //System.out.println(daysArray[0])
+       // System.out.println(weightArray[1])
 
 
-        val daysTracked = arrayOf<Int>(1,2,3,4,5,6,7,8,9,10)
-        val weightList = arrayOf<Number>(84,82,81,83,86,85,81,79,75)
-
-
-        val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* weightList), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Days Tracked")
+        val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* weightArray), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Days Tracked")
 
         val series1Format = LineAndPointFormatter(Color.BLUE, Color.BLACK, null, null)
 
@@ -81,18 +81,15 @@ class WeightTracker : AppCompatActivity() {
                 pos: FieldPosition?
             ): StringBuffer? {
                 val i = Math.round((obj as Number).toFloat())
-                return toAppendTo?.append(daysTracked[i])
+                return toAppendTo?.append(daysArray[i])
             }
 
             override fun parseObject(source: String?, pos: ParsePosition?): Any? {
                 return null
             }
-
         }
         PanZoom.attach(plot)
     }
-
-
 
     fun Home(view: android.view.View) {
         val intent = Intent(this, WeightList::class.java)
